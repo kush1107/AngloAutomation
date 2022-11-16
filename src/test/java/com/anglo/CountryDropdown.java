a149cedeb083cbbe0e5e548e8c5a4b4273e2964e
unknown <kushalparikh123@gmail.com>
2022-11-16 14:25:33 +0530

    Code updated for dropdown list func

--- src/test/java/com/anglo/CountryDropdown.java
+++ src/test/java/com/anglo/CountryDropdown.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -62,6 +63,12 @@ public WebDriver driver;
 		
 	
 	}
+	
+	 @AfterMethod
+	   	public void teardown()
+	   	{
+	   		 driver.quit();
+	   	}
 }
 
 
