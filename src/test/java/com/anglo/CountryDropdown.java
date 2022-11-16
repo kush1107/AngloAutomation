7b3650e0ab1ecd4f36d5c44563b2e69aa5a1fb84
Reyna <kushalparikh123@gmail.com>
2022-11-16 11:20:19 +0530

    Testcase uppdated

---
+++ src/test/java/com/anglo/CountryDropdown.java
@@ -0,0 +1,26 @@
+package com.anglo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class CountryDropdown {
+public WebDriver driver;
+
+	
+	@BeforeMethod
+	public void setup()
+	{
+		 driver = new ChromeDriver();
+		 driver.manage().window().maximize();
+	}
+	
+
+	@Test(groups = "To Verify CountryDropdown Functionality ")
+	public void Verify_CountryDropdown_present() 
+	{
+	}
+}
+
+
